20b35457df7067e5c866e2ddaa5660e0bccb5504
rodriguesfas <franciscosouzaacer@gmail.com>
2020-05-02 21:48:11 -0300

    first commit

---
+++ plugin_semafor.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json, os
+
+from os import path
+
+from deepnlpf.core.util import Util
+from deepnlpf.core.iplugin import IPlugin
+from deepnlpf.core.output_format import OutputFormat
+
+class Plugin(IPlugin):
+
+    def __init__(self, id_pool, lang, document, pipeline):
+
+        self._id_pool = id_pool
+        self._document = document
+
+        PATH_BASE = path.abspath(path.dirname(__file__))
+        PATH_TEMP = PATH_BASE + "/temp/"
+        
+        self.PATH_SEMAFOR = PATH_BASE
+        self.PATH_SCRIPT = './bin/runSemafor.sh'
+        self.PATH_RAW_INPUT = PATH_TEMP + "in-sentences.txt"
+        self.PATH_RAW_OUTPUT = PATH_TEMP + "out-sentences.txt"
+
+    def run(self):
+        return self.wrapper(self._document['sentences'])
+
+    def wrapper(self, dataset, threads=4):
+        '''
+            Execute the SEMAFOR.
+            @param theads (inform the number of threads that you want to execute.)
+        '''
+
+        # salve all sentences in file input.
+        self.raw_input(dataset)
+
+        # check if an output file exists and exclude.
+        os.system('cd ' + self.PATH_SEMAFOR +
+                ' && [ -e ' + self.PATH_RAW_OUTPUT + ' ] && rm ' + self.PATH_RAW_OUTPUT)
+
+        # processes a new file.
+        try:
+            os.system('cd ' + self.PATH_SEMAFOR + ' && ' + self.PATH_SCRIPT + ' ' +
+                self.PATH_RAW_INPUT + ' ' + self.PATH_RAW_OUTPUT + ' ' + str(threads))
+        except Exception as err:
+            print(err)
+
+        return self.out_format(Util().open_txt(self.PATH_RAW_OUTPUT))
+
+    def raw_input(self, dataset):
+        '''
+            Saves the sentence that will be processed in a text file.
+            @param dataset
+        '''
+        temp_file = open(self.PATH_RAW_INPUT, 'w')
+
+        for line in dataset:
+            temp_file.write(line+"\n")
+
+        temp_file.close()
+
+    def out_format(self, annotation):        
+        return OutputFormat().doc_annotation(
+            _id_pool=self._id_pool,
+            _id_dataset=self._document['_id_dataset'],
+            _id_document=self._document['_id'],
+            tool="semafor",
+            annotation=[json.loads(sent) for sent in annotation]
+        )
---
+++ requeriments.sh
@@ -0,0 +1,36 @@
+# Install SEMAFOR
+wget https://github.com/Noahs-ARK/semafor/archive/master.zip && \
+unzip master.zip && \
+rm -r master.zip && \
+mv semafor-master semafor && \
+# Config SEMAFOR
+cd semafor/bin && \
+rm -r config.sh && \
+echo -e '#!/bin/sh \
+\n export USER=$USERNAME
+\n export BASE_DIR="/home/${USER}/deepnlpf_data/plugins" \
+\n export SEMAFOR_HOME="${BASE_DIR}/semafor" \
+\n export CLASSPATH=".:${SEMAFOR_HOME}/target/Semafor-3.0-alpha-04.jar" \
+\n export JAVA_HOME_BIN="/usr/lib/jvm/java-8-openjdk-amd64/bin" \
+\n export MALT_MODEL_DIR="${BASE_DIR}/semafor/models/semafor_malt_model_20121129" \
+\n export TURBO_MODEL_DIR="{BASE_DIR}/semafor/models/turbo_20130606" \
+\n\n echo "Environment variables:" \
+\n echo "SEMAFOR_HOME=${SEMAFOR_HOME}" \
+\n echo "CLASSPATH=${CLASSPATH}" \
+\n echo "JAVA_HOME_BIN=${JAVA_HOME_BIN}" \
+\n echo "MALT_MODEL_DIR=${MALT_MODEL_DIR}" \
+'>> config.sh && \
+echo 'Download Models..' \ 
+# to path: /home/you_user/deepnlpf_data/plugin/semafor
+cd .. && \
+mkdir -p models && \
+cd models && \
+# to path: /home/you_user/deepnlpf_data/plugin/semafor/models
+wget http://www.ark.cs.cmu.edu/SEMAFOR/semafor_malt_model_20121129.tar.gz && \
+tar -vzxf semafor_malt_model_20121129.tar.gz && \
+rm -r semafor_malt_model_20121129.tar.gz && \
+echo 'Install Maven..' \
+apt-get install maven -y
+cd .. && \
+echo 'Compile SEMAFOR..' \
+mvn package
